2da8c92d34a566df291066e7a22596d5613567c3
LambFerret <dignrjrl12@gmail.com>
2021-08-21 21:41:01 +0900

    chp4 with jest devdep

--- express.js
+++ express.js
@@ -1,15 +1,37 @@
 //loading http module
 const express = require("express")
+const expressHandlebars = require('express-handlebars')
 const app = express()
 const hostname = '127.0.0.1'
 const port = 8000
+const dice = require('./lib/fortune')
 
-app.get('/', (req, res) => {
-    res.send('Hello World :)')
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => {
+    res.render('about', { dice : dice.getFortune })
 })
 
-app.use(express.static("public"))
-
-app.listen(port, hostname, () => {
-    console.log(`Server Run : http://${hostname}:${port}/`)
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 not found Ex!')
 })
+
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error Ex!')
+})
+
+app.listen(port, () => console.log(
+    `Express started : https://${hostname}:${port}`
+))
