96bce2af7e1707c222ccedfea041dd608275af15
LambFerret <dignrjrl12@gmail.com>
2021-08-23 21:57:33 +0900

    chp6 practice

--- express.js
+++ express.js
@@ -4,20 +4,66 @@ const expressHandlebars = require('express-handlebars')
 const app = express()
 const hostname = '127.0.0.1'
 const port = 8000
-const dice = require('./lib/fortune')
 const handlers = require('./lib/handlers')
+const bodyParser = require('body-parser')
 
-app.engine('handlebars', expressHandlebars({
+app.engine('hbs', expressHandlebars({
+    extname:'hbs',
     defaultLayout: 'main',
 }))
-app.set('view engine', 'handlebars')
-
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: false}))
+
+app.post('/process-contact', (req,res)=> {
+
+    console.log(`received contact from ${req.body.name} <${req.body.email}>`)
+    res.redirect(303, '10-thank you')
+})
+
 
 app.get('/', handlers.home)
+console.log("hoi!")
+
 
 app.get('/about',handlers.about )
 
+// read header
+app.get('/headers',(req,res) => {
+    res.type('text/plain')
+    const headers = Object.entries(req.headers).map(([key, value]) => `${key}: ${value}`)
+    res.send(headers.join('\n'))
+})
+
+// content rendering sample
+app.get('/greeting', (req, res)=> {
+    res.render('greeting',{
+        message: 'hellow?',
+        style: req.query.style,
+        userid: req.cookies.userid,
+        username: req.session.username
+    })
+})
+app.get('/no-layout', (req, res)=> res.render('custom-layout', {layout:null}))
+app.get('/custom-layout', (req, res)=> res.render('custom-layout', {layout:'custom'}))
+app.get('text', (req, res)=> {
+    res.type('text/plain')
+    res.send('this is a test')
+})
+app.use((err, req, res, next)=> {
+    console.error('SERVER ERROR: '+ err.message)
+    res.status(500).render('08-error', {message : "dont click me its ERROR!!"})
+})
+
+app.use((req, res) => res.status(404).render('404'))
+app.get('/error', (req, res)=>res.status(500).render('error'))
+
+// content rendering end
+
+app.get('*', (req, res) => res.send('Check out our <a href="/error">error</a> page!'))
+
+
+app.disable('x-powered-by') // response header expunged
 app.use(handlers.notFound)
 app.use(handlers.serverError)
 
