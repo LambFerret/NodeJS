3be917c4c9ea1777ac500880a2863da384ba5ebe
LambFerret <dignrjrl12@gmail.com>
2021-08-23 11:48:39 +0900

    chp5 test

--- express.js
+++ express.js
@@ -5,6 +5,7 @@ const app = express()
 const hostname = '127.0.0.1'
 const port = 8000
 const dice = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
@@ -13,24 +14,12 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', { dice : dice.getFortune })
-})
+app.get('/about',handlers.about )
 
-app.use((req, res) => {
-    res.type('text/plain')
-    res.status(404)
-    res.send('404 not found Ex!')
-})
-
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.type('text/plain')
-    res.status(500)
-    res.send('500 - Server Error Ex!')
-})
+app.use(handlers.notFound)
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started : https://${hostname}:${port}`
